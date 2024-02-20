13963a1ddc14e8753c2cef08432ef507ba1f04ab
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2024-02-19 18:46:39 -0700

    Update user button and add sign-in functionality

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -2,10 +2,11 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { navLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import router from "next/navigation";
+import { Button } from "../ui/button";
 const SideBar = () => {
   const pathname = usePathname();
   return (
@@ -23,7 +24,7 @@ const SideBar = () => {
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav_elements">
-              {navLinks.map((link) => {
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname;
                 return (
                   <li
@@ -35,20 +36,55 @@ const SideBar = () => {
                     }`}
                   >
                     <Link className="sidebar-link" href={link.route}>
-                        <Image
-                            src={link.icon}
-                            alt="logo"
-                            width={24}
-                            height={24}
-                            className={`${isActive && 'brightness-200'}`}
-                        />
-                        {link.label}
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
                     </Link>
                   </li>
                 );
               })}
             </ul>
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-grey-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton afterSignOutUrl="/" showName />
+              </li>
+            </ul>
           </SignedIn>
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Sign In</Link>
+            </Button>
+            å
+          </SignedOut>
         </nav>
       </div>
     </aside>
